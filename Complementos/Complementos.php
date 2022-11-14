939b5f7bd5df07520369f00d88d8f7d40abf3c63
J04qu1nP3r3z <joaquincpm@hotmail.com>
2022-11-13 21:37:55 -0600

    icon se actualiza

--- Complementos/Complementos.php
+++ Complementos/Complementos.php
@@ -1,6 +1,6 @@
 <title>Examen2PJCPM</title>
 <meta charset="utf-8">
-<link rel="icon" type="image/jpg" href="./JCPM.ico"/>
+<link rel="icon" type="image/jpg" href="./Complementos/JCPM.ico"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
